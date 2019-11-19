0d86051eded038b4844fe77d88394a26e4d3bebf
bruno schmidt <43834700+suika93@users.noreply.github.com>
2019-11-19 20:09:14 -0300

    Add router to display pages in the App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Header } from './components/Header'
 import { Content } from './components/Content'
+import { SituacoesPage } from './pages/Situacoes/SituacoesPage'
+import { SituacaoPage } from './pages/Situacao/SituacaoPage'
 
 const App = ()=> {
     return (
         <React.Fragment>
             <Header/>
-            <Content>
-                <h1>Conteúdo</h1>
-            </Content>
+            <BrowserRouter>
+                <Content>
+                    <Switch>
+                        <Route exact path="/" component={SituacoesPage} />
+                        <Route path="/situacao/:id" component={SituacaoPage} />
+                    </Switch>
+                </Content>
+            </BrowserRouter>
         </React.Fragment>
     )
 }
