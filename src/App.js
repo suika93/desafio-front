7f0c613184f7fc8ed7aac9da143502fcf759a5eb
bruno schmidt <43834700+suika93@users.noreply.github.com>
2019-11-19 15:04:39 -0300

    Add base layout with Header and Content components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Header } from './components/Header'
+import { Content } from './components/Content'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=> {
+    return (
+        <React.Fragment>
+            <Header/>
+            <Content>
+                <h1>Conteúdo</h1>
+            </Content>
+        </React.Fragment>
+    )
 }
 
-export default App;
+export { App }
---
+++ src/components/Content/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Content.css'
+
+const Content = (props) => {
+    return (<div className="Content">
+        {props.children}
+    </div>)
+}
+
+export { Content }
