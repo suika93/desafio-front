26b247d69d3838d8a44fc5c49ec24a740763eb72
bruno schmidt <43834700+suika93@users.noreply.github.com>
2019-11-23 23:54:16 -0300

    Remove SituationList component and add SituationFeed

--- src/components/SituationList/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import './SituationList.css'
-
-const SituationList = ({ items }) => (<div className="SituationList">{items}</div>)
-
-export { SituationList }
---
+++ src/components/SituationsFeed/index.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import { SituationItem } from '../../components/SituationItem'
+
+const INITIAL_NUMBER_OF_VISIBLE_ITEMS = 2
+
+const itemsToSituationItemComponent = 
+    (item, index) => <SituationItem key={index} name={item.name} description={item.description} />
+
+const LoadMoreButton = ({ onClick }) => {
+    return (<button onClick={onClick}>Load More</button>)
+}
+
+// inspired by Simple Load More/Pagination with React https://codepen.io/grantdotlocal/pen/zReNgE
+const SituationsFeed = ({items}) => {
+    const [numberOfVisibleItems, setNumberOfVisibleItems] = 
+        useState(INITIAL_NUMBER_OF_VISIBLE_ITEMS)
+
+    const loadMore = () => {
+        const newNumberofVisibleItems =
+            numberOfVisibleItems + 2 > items.length? items.length : numberOfVisibleItems + 2
+
+        setNumberOfVisibleItems(newNumberofVisibleItems)
+     }
+
+    return (<React.Fragment>
+        {items.slice(0, numberOfVisibleItems).map(itemsToSituationItemComponent)}
+        {items.length > numberOfVisibleItems && <LoadMoreButton onClick={loadMore} />}
+        <p>( Mostrando {numberOfVisibleItems} de {items.length})</p>
+    </React.Fragment>)
+}
+
+export { SituationsFeed }
